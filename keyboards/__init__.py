30d8ca936565569505daa27803d735984dc4a97d
Nikolay Tsybulskiy <104275092+KliffHenger@users.noreply.github.com>
2022-12-01 01:54:23 +0300

    Починка Валидации е-мейл

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,4 @@
 from keyboards import time_slot
+from keyboards import menu
+from keyboards import inline_menu
+from keyboards import english_level
--- utils/registration.py
+++ utils/registration.py
@@ -21,27 +21,27 @@ async def set_user_email(message: types.Message, state=FSMContext):
     Если почта есть в базе, то значение tg_id будет обновлено, а бот
     пустит пользователя дальше. Если нет - "тупик"
     """
-    # pattern = r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$'
+    pattern = r'^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,3}$'
     is_found = False
-    # if re.fullmatch(pattern, message.text):
-    is_found = False
-    await state.update_data(user_email=message.text)
-    find_table = table.all()
-    for index in range(len(find_table)):
-        if find_table[index]['fields']['UserEmail'] == message.text:
-            is_found = True
-            element_id = find_table[index]['id']
+    if re.fullmatch(pattern, message.text):
+        is_found = False
+        await state.update_data(user_email=message.text)
+        find_table = table.all()
+        for index in range(len(find_table)):
+            if find_table[index]['fields']['UserEmail'] == message.text:
+                is_found = True
+                element_id = find_table[index]['id']
+                await message.answer(
+                    f"Добро пожаловать, {find_table[index]['fields']['UserName']} {find_table[index]['fields']['UserSurname']}")
+                await state.finish()
+                table.update(record_id=str(element_id), fields={"UserIDTG": str(message.from_user.id)})
+                await menu(message)
+        if not is_found:
             await message.answer(
-                f"Добро пожаловать, {find_table[index]['fields']['UserName']} {find_table[index]['fields']['UserSurname']}")
+                "Вас нет в базе учеников! Свяжитесь с администрацией школы для выяснения подробностей.")
             await state.finish()
-            table.update(record_id=str(element_id), fields={"UserIDTG": str(message.from_user.id)})
-            await menu(message)
-    if not is_found:
-        await message.answer(
-            "Вас нет в базе учеников! Свяжитесь с администрацией школы для выяснения подробностей.")
-        await state.finish()
-    # else:
-    #     await message.answer(text='Введите корректное значение электронной почты.')
+    else:
+        await message.answer(text='Введите корректное значение электронной почты.')
 
 
 def register_handlers_registration(dp: Dispatcher):
